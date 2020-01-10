6328cd3fe5b54deacb7bebfa475a792a15587044
at-trangpham2 <trang.pham2@asiantech.vn>
2020-01-10 12:00:01 +0700

    fix name function and variables in js

--- exercise1/Exer.js
+++ exercise1/Exer.js
@@ -1,42 +1,43 @@
 // Câu 1:
 
-function SumTwoNumber(a,b) {
+function sumTwoNumber(a,b) {
   return (a === b) ? (3* (a+b)) : (a+b);
 }
 
-console.log(SumTwoNumber(2,2));
+console.log(sumTwoNumber(2,2));
 
 // Câu 2:
 
-function CompareTwoNumber(c) {
+function compareTwoNumber(c) {
   return (19-c >= 0) ? (19-c) : (-3 * (19-c)) ;
 }
-console.log(CompareTwoNumber(12));
+console.log(compareTwoNumber(12));
 
 
 // Câu 3:
-var A = [];
-function DivideNumber3(str1) {
+
+function divideNumber3(str1) {
+  var A = [];
   for (i=0; i<=9; i++) {
-    var subA = str1.replace('*',i);
+    subA = str1.replace('*',i);
     if (parseInt(subA) % 3 === 0) {
       A.push(subA);
     }
   }
   return A;
 }
-console.log(DivideNumber3('1*9'));
+console.log(divideNumber3('1*9'));
 
 // Câu 4:
-var B = [];
 
-function DivideNumber6(str2) {
+function divideNumber6(str2) {
+  var B = [];
   for (i=0; i<=9; i++) {
-    var subB = str2.replace('*',i);
+    subB = str2.replace('*',i);
     if (parseInt(subB) % 6 === 0) {
       B.push(subB);
     }
   }
   return B;
 }
-console.log(DivideNumber6('1234567890*'));
+console.log(divideNumber6('1234567890*'));
