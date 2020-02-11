af9c884a73f2ecd2dad6a61cc6252cf467a9612a
at-trangpham2 <trang.pham2@asiantech.vn>
2020-02-11 13:35:23 +0700

    fix js and add file Ex_Article.js

--- Exercise3/Ex_news.js
+++ Exercise3/Ex_news.js
@@ -1,54 +1,87 @@
-var boxComment = [];
+var boxComment;
 
-function showComment() {
+//add event for addComment button
+function eventForAddCommentButton() {
   var boxComment = JSON.parse(localStorage.getItem("boxComment"));
-  var displayComment = document.getElementById("js-display-comment");
-  displayComment.innerHTML = "";
-  for (i = 0; i < boxComment.length; i++) {
-    displayComment.innerHTML +=
-      "<ul class='menu-list-comment'>" +
-      '<li class="list-comment">' +
-      '<img class="avatar" src="images/avatar.png" >' +
-      '<div class="user-content">' +
-      "<h4>Trang Pham</h4>" +
-      boxComment[i] +
-      "</div>" +
-      '<button class="btn-remove-comment" data-id ="' +
-      i +
-      '" >X</button>' +
-      "</li>" +
-      "</ul>";
+  if (!boxComment) {
+    boxComment = [];
   }
-  removeComment();
-}
-// showComment();
 
-function addComment() {
   document
     .getElementById("js-comment-btn")
     .addEventListener("click", function() {
       var inputComment = document.getElementById("js-enter-comment").value;
       if (inputComment) {
-        boxComment.push(inputComment);
+        var comment = {
+          idComment: boxComment.length,
+          commenti: inputComment,
+          idArticle: 1,
+          isDelete: false,
+          user: {
+            id: 1,
+            avatar: "images/avatar.png",
+            name: "Trang Pham"
+          }
+        };
+
+        boxComment.push(comment);
+
         localStorage.setItem("boxComment", JSON.stringify(boxComment));
-        showComment();
+        renderComment();
         document.getElementById("js-enter-comment").value = "";
       }
     });
 }
-addComment();
+eventForAddCommentButton();
 
-function removeComment() {
+//show comment
+function renderComment() {
+  var boxComment = JSON.parse(localStorage.getItem("boxComment"));
+  console.log(boxComment);
+  var displayComment = document.getElementById("js-display-comment");
+  displayComment.innerHTML = "";
+
+  for (i = 0; i < boxComment.length; i++) {
+    if (boxComment[i].isDelete === false) {
+      displayComment.innerHTML +=
+        '<ul class="menu-list-comment">' +
+        '<li class="list-comment">' +
+        '<img class="avatar" src=' +
+        boxComment[i].user.avatar +
+        " >" +
+        '<div class="user-content">' +
+        "<h4>" +
+        boxComment[i].user.name +
+        "</h4>" +
+        boxComment[i].commenti +
+        "</div>" +
+        '<button class="btn-remove-comment" data-id ="' +
+        boxComment[i].idComment +
+        '" >X</button>' +
+        "</li>" +
+        "</ul>";
+    }
+  }
+  eventForRemoveCommentButton();
+}
+renderComment();
+
+//add event for removeComment button
+function eventForRemoveCommentButton() {
+  var boxComment = JSON.parse(localStorage.getItem("boxComment"));
   var btnRm = document.getElementsByClassName("btn-remove-comment");
-  for (i = 0; i < btnRm.length; i++) {
-    btnRm[i].addEventListener("click", function(brm) {
-      var brmove = brm.target.dataset.id;
-      // for (var j = 0; j < boxComment.length; j++) {
-      //   if (brmove === boxComment[j]) {
-      boxComment.splice(brmove, 1);
+  for (j = 0; j < btnRm.length; j++) {
+    btnRm[j].addEventListener("click", function(brm) {
+      var bremove = +brm.target.dataset.id;
+      console.log(bremove);
+      for (i = 0; i < boxComment.length; i++) {
+        if (bremove === boxComment[i].idComment) {
+          boxComment[i].isDelete = true;
+        }
+      }
       localStorage.setItem("boxComment", JSON.stringify(boxComment));
-      showComment();
+      renderComment();
     });
   }
 }
-
+eventForRemoveCommentButton();
